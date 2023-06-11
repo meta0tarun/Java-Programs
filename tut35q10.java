b45a417754cace8e92b4c9e4786d6e2930f36c7e
Tarun Kushwaha <110657234+meta0tarun@users.noreply.github.com>
2023-06-11 07:58:55 +0530

    Add files via upload

---
+++ tut35q10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class tut35q10 {
+
+    static void prtstr(int a){
+        if(a==0){
+            return;
+        }
+        
+        prtstr(a-1);
+
+        for(int i=0; i<a; i++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        System.out.print("Enter the number : ");
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        prtstr(num);
+
+    }
+}
